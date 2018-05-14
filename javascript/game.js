51488e48b720a93d970cc7b2fc843983c800eee9
Romeo Amaro <romeoamaro@Romeos-MBP.attlocal.net>
2018-05-13 23:08:59 -0700

    update

---
+++ javascript/game.js
@@ -0,0 +1,36 @@
+var random_result;
+var lost;
+var win;
+
+
+
+
+
+
+
+for( var i = 0; i < 4; i++){
+    var random= Math.floor(Math.random () * 12);
+    console.log(random);
+}
+
+    var Crystals = $("<h2></h2>").text(random_result);
+       
+       $("#random-number").append(Crystals);
+         
+      ;
+
+
+//$(".Crystals").append(Crystal);
+
+
+
+
+// I am  missing the following:
+
+
+
+// create an random number generador 
+//create a function to display the wins 
+//create a fuction to display the lost
+//create a fuction to display the total score 
+//make the 
